744591ea3bf6eda5705bf35df6f8cc06b2d8a9a3
Visoth YOUK-SIM <115548934+visothyouksim@users.noreply.github.com>
2024-06-28 15:57:00 +0200

    Update DarkTransactionService.java

--- src/main/java/com/example/PayMe/service/DarkTransactionService.java
+++ src/main/java/com/example/PayMe/service/DarkTransactionService.java
@@ -1,4 +1,23 @@
 package com.example.PayMe.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.PayMe.model.User;
+import com.example.PayMe.repository.TransactionRepository;
+import com.example.PayMe.repository.UserRepository;
+
+@Service
 public class DarkTransactionService {
+
+    @Autowired
+    TransactionRepository transactionRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Object getDarkTransactionsListByUserName(String userName) {
+        User user = userRepository.getUserByName(userName);
+        return transactionRepository.findAllBySender(user);
+    }
 }
